41a79e144329a20f3e8ad28271e57cbeee245b83
Denis Popov <deniolp@gmail.com>
2018-04-05 13:06:14 +0300

    Оптимизация кода - создание констант и вынесение генерации данных объекта в отдельную функцию generatePhoto

--- js/pictures.js
+++ js/pictures.js
@@ -1,27 +1,40 @@
 'use strict';
 
 var photoList = [];
-var commentsList = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var descriptionList = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
+var LIMIT_PHOTOS = 25;
+var COMMENTS_LIST = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var DESCRIPTION_LIST = ['Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'];
 
-var getRandom = function getRandomInt(min, max) {
+var generateRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getComments = function (quantityOfComments) {
+var generateComments = function (quantityOfComments) {
   var comments = [];
   for (var i = 1; i <= quantityOfComments; i++) {
-    comments.push(commentsList[getRandom(0, commentsList.length - 1)]);
+    comments.push(COMMENTS_LIST[generateRandomNumber(0, COMMENTS_LIST.length - 1)]);
   } return comments;
 };
 
-for (var i = 1; i <= 25; i++) {
-  photoList.push(
-      {
-        url: 'photos/' + i,
-        likes: getRandom(15, 200),
-        comments: getComments(getRandom(1, 2)),
-        description: descriptionList[getRandom(0, descriptionList.length - 1)]
-      }
-  );
+var generatePhoto = function (index) {
+  return {
+    url: 'photos/' + index,
+    likes: generateRandomNumber(15, 200),
+    comments: generateComments(generateRandomNumber(1, 2)),
+    description: DESCRIPTION_LIST[generateRandomNumber(0, DESCRIPTION_LIST.length - 1)]
+  };
+};
+
+for (var i = 1; i <= LIMIT_PHOTOS; i++) {
+  photoList.push(generatePhoto(i));
 }
