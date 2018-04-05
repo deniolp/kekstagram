6ebb7bdb9fa42145cc6d7ba2af94bc9e95079039
Denis Popov <deniolp@gmail.com>
2018-04-05 14:52:56 +0300

    Создание DOM элементов с соответствующими данными и фото и их отрисовка с помощью DocumentFragment

--- js/pictures.js
+++ js/pictures.js
@@ -1,19 +1,27 @@
 'use strict';
 
-var photoList = [];
 var LIMIT_PHOTOS = 25;
-var COMMENTS_LIST = ['Всё отлично!',
+var COMMENTS_LIST = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var DESCRIPTION_LIST = ['Тестим новую камеру!',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var DESCRIPTION_LIST = [
+  'Тестим новую камеру!',
   'Затусили с друзьями на море',
   'Как же круто тут кормят',
   'Отдыхаем...',
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!'];
+  'Вот это тачка!'
+];
+
+var photoList = [];
+var template = document.querySelector('#picture').content.querySelector('a');
+var pictures = document.querySelector('.pictures');
+var fragment = document.createDocumentFragment();
 
 var generateRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,18 +31,29 @@ var generateComments = function (quantityOfComments) {
   var comments = [];
   for (var i = 1; i <= quantityOfComments; i++) {
     comments.push(COMMENTS_LIST[generateRandomNumber(0, COMMENTS_LIST.length - 1)]);
-  } return comments;
+  }
+  return comments;
 };
 
 var generatePhoto = function (index) {
   return {
-    url: 'photos/' + index,
+    url: 'photos/' + index + '.jpg',
     likes: generateRandomNumber(15, 200),
     comments: generateComments(generateRandomNumber(1, 2)),
     description: DESCRIPTION_LIST[generateRandomNumber(0, DESCRIPTION_LIST.length - 1)]
   };
 };
 
-for (var i = 1; i <= LIMIT_PHOTOS; i++) {
-  photoList.push(generatePhoto(i));
+for (var i = 0; i < LIMIT_PHOTOS; i++) {
+  photoList.push(generatePhoto(i + 1));
+  var photo = template.cloneNode(true);
+  var img = photo.querySelector('img');
+  img.src = photoList[i].url;
+  var photoStat = photo.querySelector('.picture__stat--likes');
+  photoStat.textContent = photoList[i].likes;
+  var commentStat = photo.querySelector('.picture__stat--comments');
+  commentStat.textContent = photoList[i].comments.length;
+  fragment.appendChild(photo);
 }
+
+pictures.appendChild(fragment);
