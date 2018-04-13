fb62ee7a56c7be45486372bc1797a6b8d49da4da
Denis Popov <deniolp@gmail.com>
2018-04-13 22:55:31 +0300

    Исправление функции caclulateScrollBarWidth

--- js/pictures.js
+++ js/pictures.js
@@ -159,13 +159,13 @@ var createStyleEffect = function (effect) {
   }
 };
 
-var caclulateScrollBarWidth = function (scaleLineElement) {
+var caclulateScrollBarWidth = function () {
   return scaleLineElement.getBoundingClientRect().width;
 };
 
 var getEffectIntensity = function (value) {
-  var pinPosition = value - (document.documentElement.clientWidth - caclulateScrollBarWidth(scaleLine)) / 2;
-  effectIntensity = Math.floor(pinPosition / (caclulateScrollBarWidth(scaleLine) / 100));
+  var pinPosition = value - (document.documentElement.clientWidth - caclulateScrollBarWidth()) / 2;
+  effectIntensity = Math.floor(pinPosition / (caclulateScrollBarWidth() / 100));
 
   scaleValueInputElement.value = effectIntensity;
 
@@ -193,7 +193,7 @@ var effectHeatElement = uploadFormElement.querySelector('#effect-heat');
 var scalePinElement = uploadFormElement.querySelector('.scale__pin');
 var scaleValueInputElement = uploadFormElement.querySelector('.scale__value');
 var scaleElement = uploadFormElement.querySelector('.img-upload__scale');
-var scaleLine = uploadFormElement.querySelector('.scale__line');
+var scaleLineElement = uploadFormElement.querySelector('.scale__line');
 
 var resizeButtonMinus = uploadFormElement.querySelector('.resize__control--minus');
 var resizeButtonPlus = uploadFormElement.querySelector('.resize__control--plus');
