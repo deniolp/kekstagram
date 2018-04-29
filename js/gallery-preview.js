9ffa86ee90924da746f4d3a1d9092c2c0b8578f1
Denis Popov <deniolp@gmail.com>
2018-04-29 11:51:25 +0300

    Реализация добавления пяти комментариев по клику на 'Загрузить еще' пока с одинаковыми комментариями

--- js/gallery-preview.js
+++ js/gallery-preview.js
@@ -4,27 +4,38 @@
 
   var KEYCODE_ESC = 27;
   var AVATAR_LIMIT = 6;
+  var COMMENTS_DOSE = 5;
 
   var showPicture = function (data, element) {
     var commentBlockTemplate = element.querySelector('.social__comment').cloneNode(true);
     var commentsBlock = element.querySelector('.social__comments');
     var fragmentComment = document.createDocumentFragment();
+    var loadMoreButton = element.querySelector('.social__comment-loadmore');
+    var commentCountElement = element.querySelector('.social__comment-count');
     var commentBlock;
 
+    var addComments = function () {
+      for (var i = 0; i < COMMENTS_DOSE; i++) {
+        commentBlock = commentBlockTemplate.cloneNode(true);
+        commentBlock.querySelector('img').src = 'img/avatar-' + (i % AVATAR_LIMIT + 1) + '.svg';
+        commentBlock.lastChild.textContent = data.comments[i];
+        fragmentComment.appendChild(commentBlock);
+      }
+    };
+
     element.classList.remove('hidden');
-    element.querySelector('.social__comment-count').classList.add('visually-hidden');
-    element.querySelector('.social__comment-loadmore').classList.add('visually-hidden');
     element.querySelector('.big-picture__img').querySelector('img').src = data.url;
     element.querySelector('.likes-count').textContent = data.likes;
     element.querySelector('.comments-count').textContent = data.comments.length;
     element.querySelector('.social__caption').textContent = data.description;
 
-    for (var i = 0; i < data.comments.length; i++) {
-      commentBlock = commentBlockTemplate.cloneNode(true);
-      commentBlock.querySelector('img').src = 'img/avatar-' + (i % AVATAR_LIMIT + 1) + '.svg';
-      commentBlock.lastChild.textContent = data.comments[i];
-      fragmentComment.appendChild(commentBlock);
-    }
+    var currentCommentNumber = COMMENTS_DOSE;
+    addComments();
+
+    loadMoreButton.addEventListener('click', function () {
+      addComments();
+      commentsBlock.appendChild(fragmentComment);
+    });
 
     commentsBlock.innerHTML = '';
     commentsBlock.appendChild(fragmentComment);
