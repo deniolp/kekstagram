5f6875d6a2ef6ab14e9bbc386c6520d572f252f0
Denis Popov <deniolp@gmail.com>
2018-04-30 14:10:05 +0300

    Изменение способа удаления сообщения об ошибке

--- js/error-message.js
+++ js/error-message.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var ERROR_TIMEOUT = 5000;
+
   var errorElement = document.createElement('div');
 
   errorElement.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; height: 50px; padding-top: 10px; position: absolute; left: 0; right: 0; font-size: 30px';
@@ -13,11 +15,9 @@
       if (!document.body.contains(errorElement)) {
         document.body.insertAdjacentElement('afterbegin', errorElement);
       }
-    },
-    clear: function () {
-      if (document.body.contains(errorElement)) {
-        document.body.removeChild(errorElement);
-      }
+      setTimeout(function () {
+        errorElement.classList.add('hidden');
+      }, ERROR_TIMEOUT);
     }
   };
 })();
