9720f656df31af321b919a98937328728cd53604
Denis Popov <deniolp@gmail.com>
2018-05-05 09:09:35 +0300

    Исправление error-message

--- js/error-message.js
+++ js/error-message.js
@@ -1,40 +1,27 @@
 'use strict';
 
 (function () {
-  var ERROR_TIMEOUT = 7500;
+  var ERROR_TIMEOUT = 5000;
   var uploadFormElement = document.querySelector('.img-upload__form');
   var uploadImageElement = uploadFormElement.querySelector('.img-upload__overlay');
   var uploadFileInputElement = uploadFormElement.querySelector('#upload-file');
-  var errorCenterElement = document.querySelector('.img-upload__message--error');
+  var errorElement = document.querySelector('.img-upload__message--error');
+  var errorLinksElement = document.querySelector('.error__links');
 
   window.errorMessage = {
     show: function (errorMessage) {
-      var errorElement = document.createElement('div');
+      errorElement.classList.remove('hidden');
+      uploadImageElement.classList.add('hidden');
+      uploadFileInputElement.value = '';
 
-      if (errorCenterElement.classList.contains('hidden')) {
-        errorCenterElement.classList.remove('hidden');
-        uploadImageElement.classList.add('hidden');
-        uploadFileInputElement.value = '';
+      if (errorElement.contains(errorLinksElement)) {
+        errorElement.removeChild(errorLinksElement);
       }
-
-      errorElement.style.backgroundColor = 'red';
-      errorElement.style.margin = '0 auto';
-      errorElement.style.textAlign = 'center';
-      errorElement.style.height = '50px';
-      errorElement.style.paddingTop = '10px';
-      errorElement.style.position = 'absolute';
-      errorElement.style.top = 0;
-      errorElement.style.left = 0;
-      errorElement.style.right = 0;
-      errorElement.style.fontSize = '30px';
-      errorElement.style.zIndex = 100;
 
       errorElement.textContent = errorMessage;
 
-      document.body.appendChild(errorElement);
-
       setTimeout(function () {
-        document.body.removeChild(errorElement);
+        errorElement.classList.add('hidden');
       }, ERROR_TIMEOUT);
     }
   };
