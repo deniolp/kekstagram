ccb31125fcf3ad290e369a4079b5e97ae3d63d24
Denis Popov <deniolp@gmail.com>
2018-05-08 21:42:43 +0300

    Изменение в gallery-filter.js

--- js/gallery-filter.js
+++ js/gallery-filter.js
@@ -1,47 +1,43 @@
 'use strict';
 
 (function () {
-
   var Filter = {
     POPULAR: 'filter-popular',
     DISCUSSED: 'filter-discussed',
     RANDOMED: 'filter-randomed'
   };
 
+  var compareFunctions = {};
+
+  compareFunctions[Filter.POPULAR] = function (a, b) {
+    if (a.likes > b.likes) {
+      return -1;
+    }
+    if (a.likes < b.likes) {
+      return 1;
+    }
+    return 0;
+  };
+
+  compareFunctions[Filter.DISCUSSED] = function (a, b) {
+    if (a.comments.length > b.comments.length) {
+      return -1;
+    }
+    if (a.comments.length < b.comments.length) {
+      return 1;
+    }
+    return 0;
+  };
+
+  compareFunctions[Filter.RANDOMED] = function () {
+    return Math.random() - 0.5;
+  };
+
   window.galleryFilter = {
     addFilteredPictures: function (pictures, sorterName) {
-      switch (sorterName) {
-        case Filter.POPULAR:
-          sortBy = function (a, b) {
-            if (a.likes > b.likes) {
-              return -1;
-            }
-            if (a.likes < b.likes) {
-              return 1;
-            }
-            return 0;
-          };
-          break;
-        case Filter.DISCUSSED:
-          sortBy = function (a, b) {
-            if (a.comments.length > b.comments.length) {
-              return -1;
-            }
-            if (a.comments.length < b.comments.length) {
-              return 1;
-            }
-            return 0;
-          };
-          break;
-        case Filter.RANDOMED:
-          sortBy = function () {
-            return Math.random() - 0.5;
-          };
-      }
-
-      var sortBy;
+      var compareFunction = compareFunctions[sorterName];
       var copiedPictures = pictures.slice();
-      var sortedPictures = sortBy ? copiedPictures.sort(sortBy) : copiedPictures;
+      var sortedPictures = compareFunction ? copiedPictures.sort(compareFunction) : copiedPictures;
 
       window.gallery.addPictures(sortedPictures);
     }
